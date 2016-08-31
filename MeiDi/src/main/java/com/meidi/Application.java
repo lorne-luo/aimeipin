3fd34a95645e962140386e13285c12b734834b1e
Tao <taoluo@combitel.com.au>
2016-08-31 14:05:39 +1000

    enable scheduling

--- MeiDi/src/main/java/com/meidi/Application.java
+++ MeiDi/src/main/java/com/meidi/Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by luanpeng on 16/3/10.
  */
+@EnableScheduling
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
