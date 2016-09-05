8b572917337a06365837e97f961473ab67adf5ba
Tao <git@luotao.net>
2016-09-05 20:52:44 +1000

    query all city's commodity

--- MeiDi/src/main/resources/static/js/weixin/classification.js
+++ MeiDi/src/main/resources/static/js/weixin/classification.js
@@ -15,7 +15,7 @@ $(function () {
  *
  * @param categoryId 项目分类 －1 查所有
  * @param pageNumber 页码 1 表示第一页 0 表示继续查询
- * @param city 地区 －1 查所有
+ * @param city 地区 －1 查所有, －2 查非韩国所有
  */
 function setProjectFlagAndGetProject(categoryId, city, pageNumber, tag) {
     $('.t' + tag).parent().find('.active').removeClass('active');
