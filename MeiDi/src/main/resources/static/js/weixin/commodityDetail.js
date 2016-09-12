7c51ba2fb22ede767494e45e626f01dcaa88bb49
Tao <git@luotao.net>
2016-09-12 20:56:47 +1000

    fix 去参团button in commodity detail

--- MeiDi/src/main/resources/static/js/weixin/commodityDetail.js
+++ MeiDi/src/main/resources/static/js/weixin/commodityDetail.js
@@ -186,8 +186,8 @@ function createTable_youlike(commodityList) {
                 '    <div class=" pr price">' +
                 '      <div class="lefticon "></div>' +
                 '      <div class="righticon cleafix pr ">' +
-                '        <span class=" ml50 mr10 fl fs16">原价：<del>' + commodity.price / 100 + '</del></span>' +
-                '        <span class="fs24 fl">' + commodity.peopleNumber + '人团：¥' + commodity.discountPrice / 100 + '</span>' +
+                '        <span class=" ml50 mr10 fl fs12">原价：<del>' + commodity.price / 100 + '</del></span>' +
+                '        <span class="fs16 fl">' + commodity.peopleNumber + '人团：¥' + commodity.discountPrice / 100 + '</span>' +
                 '        <a href="' + BASE_JS_URL + '/business/commodityDetailPage/' + commodity.id + '"></a>' +
                 '      </div>' +
                 '    </div>' +
@@ -211,8 +211,8 @@ function createTable_youlike(commodityList) {
                 '  <div class=" pr price">' +
                 '    <div class="lefticon fu"></div>' +
                 '      <div class="righticon cleafix pr qiang">' +
-                '        <span class=" ml50 mr10 fl fs16">原价：<del>' + commodity.price / 100 + '</del></span>' +
-                '        <span class="fs24 fl">现价：¥' + commodity.discountPrice / 100 + '</span>' +
+                '        <span class=" ml50 mr10 fl fs12">原价：<del>' + commodity.price / 100 + '</del></span>' +
+                '        <span class="fs16 fl">现价：¥' + commodity.discountPrice / 100 + '</span>' +
                 '        <a href="' + BASE_JS_URL + '/business/commodityDetailPage/' + commodity.id + '"></a>' +
                 '      </div>' +
                 '    </div>' +
@@ -238,8 +238,8 @@ function createTable_youlike(commodityList) {
                 '    <div class=" pr price">' +
                 '      <div class="lefticon sale"></div>' +
                 '      <div class="righticon cleafix pr qiang">' +
-                '        <span class=" ml50 mr10 fl fs16">原价：<del>' + commodity.price / 100 + '</del></span>' +
-                '        <span class="fs24 fl">现价：¥' + commodity.discountPrice / 100 + '</span>' +
+                '        <span class=" ml50 mr10 fl fs12">原价：<del>' + commodity.price / 100 + '</del></span>' +
+                '        <span class="fs16 fl">现价：¥' + commodity.discountPrice / 100 + '</span>' +
                 '        <a href="' + BASE_JS_URL + '/business/commodityDetailPage/' + commodity.id + '"></a>' +
                 '      </div>' +
                 '    </div>' +
